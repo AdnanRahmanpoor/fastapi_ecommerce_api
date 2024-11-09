fc91e9b345555b60d7fa63e12b01d8a7e564247d
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-11-09 15:47:10 +0400

    schemas

---
+++ app/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel # validation and serialization of data for api
+from typing import Optional
+
+# data required to create User model: username and password and their type is string
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+# core attributes of a product
+class ProductBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    category_id: int
+
+# inheriting from productbase to create a new product
+class ProductCreate(ProductBase):
+    pass
+
+# this is for retreiving product data from productbase, and assigns an 'id' to each product response
+class ProductResponse(ProductBase):
+    id: int
